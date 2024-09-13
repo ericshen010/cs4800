f9d703a7e7b366de9ce4ee44c140a47e40205094
ericshen010 <ericshen010@gmail.com>
2024-09-13 11:30:40 -0700

    changes

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
