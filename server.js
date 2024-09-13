26a1716f290ae005b7112cc3a731be8a2f7a50c0
ericshen010 <ericshen010@gmail.com>
2024-09-13 11:33:53 -0700

    removed duplicated mongoose

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-const mongoose = require('mongoose');
-require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
